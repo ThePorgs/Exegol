c7d0e8c06fd3932d3ce2efb52a6e1e3e1c7ea124
Dramelac <Dramelac@users.noreply.github.com>
2022-04-17 15:58:44 +0200

    User config upgrade mode + add resources submodule

--- exegol/utils/UserConfig.py
+++ exegol/utils/UserConfig.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import yaml
 import yaml.parser
@@ -17,10 +17,12 @@ class UserConfig(metaclass=MetaSingleton):
         # Config file options
         self.__exegol_path = Path().home() / ".exegol"
         self.__config_file_path = self.__exegol_path / "config.yml"
+        self.__config_upgrade = False
 
         # Defaults User config
         self.private_volume_path = self.__exegol_path / "workspaces"
         self.shared_resources_path = str(self.__exegol_path / "shared-resources")
+        self.exegol_resources_path = self.__default_resource_location('exegol-resources')
 
         # process
         self.__load_file()
@@ -31,16 +33,23 @@ class UserConfig(metaclass=MetaSingleton):
             os.mkdir(self.__exegol_path)
         if not self.__config_file_path.is_file():
             logger.verbose(f"Creating default exegol config: {self.__config_file_path}")
-            self.__create_default_config()
+            self.__create_config_file()
         else:
             self.__parse_config()
+            if self.__config_upgrade:
+                logger.verbose("Upgrading config file")
+                self.__create_config_file()
 
-    def __create_default_config(self):
+    def __create_config_file(self):
+        # TODO update comment for docker volume
         config = f"""# Exegol configuration
         
 volumes:
     # Changing the shared resources path must be set before creating any exegol container (and remove the docker volume {ConstantConfig.COMMON_SHARE_NAME} if exists)
     shared_resources_path: {self.shared_resources_path}
+    
+    # Exegol static resources location (can be change at any moment but the update will not affect already created containers)
+    exegol_resources_path: {self.exegol_resources_path}
     
     # Changing the location of the private workspace parent directory can be done at any moment but the change will not affect already created containers
     private_workspace_path: {self.private_volume_path}
@@ -52,6 +61,27 @@ volumes:
         with open(self.__config_file_path, 'w') as file:
             file.write(config)
 
+    def __default_resource_location(self, folder_name: str) -> Path:
+        local_src = ConstantConfig.src_root_path_obj / folder_name
+        if local_src.is_dir():
+            # If exegol is clone from github, exegol-resources submodule is accessible from root src
+            return local_src
+        else:
+            # Default path for pip installation
+            return self.__exegol_path / folder_name
+
+    def __load_config_path(self, data: dict, config_name: str, default: Union[Path, str]) -> Union[Path, str]:
+        try:
+            result = data.get(config_name)
+            if result is None:
+                logger.debug(f"Config {config_name} has not been found in exegol config file. Config file will be upgrade.")
+                self.__config_upgrade = True
+                return default
+            return Path(result).expanduser()
+        except TypeError:
+            logger.error(f"Error while loading {config_name}! Using default config.")
+        return default
+
     def __parse_config(self):
         with open(self.__config_file_path, 'r') as file:
             try:
@@ -59,24 +89,21 @@ volumes:
             except yaml.parser.ParserError:
                 data = {}
                 logger.error("Error while parsing exegol config file ! Check for syntax error.")
+        # bug: logger verbosity not set at this time
         logger.debug(data)
         volumes_data = data.get("volumes", {})
+        # Catch existing but empty section
         if volumes_data is None:
             volumes_data = {}
-        try:
-            self.shared_resources_path = str(Path(volumes_data.get('shared_resources_path',
-                                                                   self.shared_resources_path)).expanduser())
-        except TypeError:
-            logger.error("Error while loading shared_resources path! Using default config.")
-        try:
-            self.private_volume_path = Path(volumes_data.get('private_workspace_path',
-                                                             self.private_volume_path)).expanduser()
-        except TypeError:
-            logger.error("Error while loading private_workspace path! Using default config.")
+        self.shared_resources_path = str(self.__load_config_path(volumes_data, 'shared_resources_path', self.shared_resources_path))
+        self.private_volume_path = self.__load_config_path(volumes_data, 'private_workspace_path', self.private_volume_path)
+        self.exegol_resources_path = self.__load_config_path(volumes_data, 'exegol_resources_path', self.exegol_resources_path)
 
     def get_configs(self) -> List[str]:
+        """User configs getter each options"""
         configs = [
             f"Shared resources = {self.shared_resources_path}",
+            f"Exegol resources = {self.exegol_resources_path}",
             f"Private workspace = {self.private_volume_path}"
         ]
         # TUI can't be called from here to avoid circular importation
