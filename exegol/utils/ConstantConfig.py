31d82bb54a5f34df273c474504638304a5d65f0f
Dramelac <Dramelac@users.noreply.github.com>
2022-04-26 17:34:45 +0200

    Upgrade beta version
    Signed-off-by: Dramelac <Dramelac@users.noreply.github.com>

--- exegol/utils/ConstantConfig.py
+++ exegol/utils/ConstantConfig.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class ConstantConfig:
     """Constant parameters information"""
     # Exegol Version
-    version: str = "4.0.0b3"
+    version: str = "4.0.0b4"
 
     # OS Dir full root path of exegol project
     src_root_path_obj: Path = Path(__file__).parent.parent.parent.resolve()
