3df15356904cf215ef3f840843d7ec3003652f11
Dramelac <dramelac@users.noreply.github.com>
2023-03-09 13:43:03 +0100

    Fix exec complete order

--- exegol/console/cli/ExegolCompleter.py
+++ exegol/console/cli/ExegolCompleter.py
@@ -1,27 +1,26 @@
 from argparse import Namespace
-from typing import List, Tuple
+from typing import Tuple
 
 from argcomplete import warn
 
 from exegol.utils.DockerUtils import DockerUtils
 
 
-def ContainerCompleter(prefix: str, **kwargs) -> List[str]:
+def ContainerCompleter(prefix: str, parsed_args: Namespace, **kwargs) -> Tuple[str, ...]:
     """Function to dynamically load a container list for CLI autocompletion purpose"""
     data = [c.name for c in DockerUtils.listContainers()]
     for obj in data:
         # filter data if needed
         if prefix and not obj.startswith(prefix):
             data.remove(obj)
-    return data
+    return tuple(data)
 
 
-def ImageCompleter(prefix: str, **kwargs) -> List[str]:
+def ImageCompleter(prefix: str, parsed_args: Namespace, **kwargs) -> Tuple[str, ...]:
     """Function to dynamically load an image list for CLI autocompletion purpose"""
-    parsed_args: Namespace = kwargs.get('parsed_args')
     # Skip image completer when container hasn't been selected first (because parameters are all optional)
     if parsed_args is not None and str(parsed_args.action) == "start" and parsed_args.containertag is None:
-        return []
+        return ()
     # data = [i.getName() for i in DockerUtils.listImages()]
     # TODO create a fast-load image listing (need image caching)
     data = ["full", "nightly", "ad", "web", "light", "osint"]
@@ -29,15 +28,17 @@ def ImageCompleter(prefix: str, **kwargs) -> List[str]:
         # filter data if needed
         if prefix and not obj.startswith(prefix):
             data.remove(obj)
-    return data
+    return tuple(data)
 
 
-def HybridContainerImageCompleter(prefix: str, parsed_args: Namespace, **kwargs) -> List[str]:
+def HybridContainerImageCompleter(prefix: str, parsed_args: Namespace, **kwargs) -> Tuple[str, ...]:
+    # warn(parsed_args)
+    if parsed_args.selector is None and parsed_args.exec is not None and len(parsed_args.exec) > 0:
+        return ()
     if parsed_args.tmp:
-        return ImageCompleter(prefix, **kwargs)
+        return ImageCompleter(prefix, parsed_args, **kwargs)
     else:
-        warn("Container mode")
-        return ContainerCompleter(prefix, **kwargs)
+        return ContainerCompleter(prefix, parsed_args, **kwargs)
 
 
 def VoidCompleter(**kwargs) -> Tuple:
