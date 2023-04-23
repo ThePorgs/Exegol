e01b49b998a7c6e20b62da23ea185ac099d9da78
Dramelac <dramelac@users.noreply.github.com>
2023-04-24 00:05:48 +0200

    New dev beta version

--- exegol/config/ConstantConfig.py
+++ exegol/config/ConstantConfig.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class ConstantConfig:
     """Constant parameters information"""
     # Exegol Version
-    version: str = "4.1.2b1"
+    version: str = "4.1.2b2"
 
     # Exegol documentation link
     documentation: str = "https://exegol.rtfd.io/"
