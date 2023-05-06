0c023f671997ac763c8287ef8dd41595ff2746fb
Dramelac <dramelac@users.noreply.github.com>
2023-05-07 00:38:58 +0200

    Add py 3.11 label

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
