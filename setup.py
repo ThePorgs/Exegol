d82056eb1c19c29459f68aadbc1d58f503c0bcef
Dramelac <dramelac@users.noreply.github.com>
2023-12-22 16:06:51 +0100

    Add py3.12 tests

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
