03885e7380cf154b393fb1f3e644bab904e5db56
Dramelac <dramelac@users.noreply.github.com>
2024-03-17 20:41:57 +0100

    Fix comment sponsor

--- exegol/manager/ExegolManager.py
+++ exegol/manager/ExegolManager.py
@@ -234,8 +234,7 @@ class ExegolManager:
     @classmethod
     def print_sponsors(cls):
         """Show exegol sponsors"""
-        #logger.success(
-            """We thank [link=https://www.capgemini.com/fr-fr/carrieres/offres-emploi/][blue]Capgemini[/blue][/link] for supporting the project [bright_black](helping with dev)[/bright_black] :pray:""")
+        #logger.success("""We thank [link=https://www.capgemini.com/fr-fr/carrieres/offres-emploi/][blue]Capgemini[/blue][/link] for supporting the project [bright_black](helping with dev)[/bright_black] :pray:""")
         #logger.success("""We thank [link=https://www.hackthebox.com/][green]HackTheBox[/green][/link] for sponsoring the [bright_black]multi-arch[/bright_black] support :green_heart:""")
         pass
 
