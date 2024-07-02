71476a726d1b174b8b483058ff849573e64b8a59
Ranma <54248704+cHJlaXpoZXI@users.noreply.github.com>
2024-07-02 15:36:20 +0000

    Update ContainerConfig.py

--- exegol/model/ContainerConfig.py
+++ exegol/model/ContainerConfig.py
@@ -1008,7 +1008,7 @@ class ContainerConfig:
                         msg += " Your exegol installation cannot be stored under this directory."
                         logger.critical(msg)
                     raise CancelOperation(msg)
-                if path_match.startswith("/etc/"):
+                if path_match.startswith("/etc/") and EnvInfo.isDockerDesktop():
                     path_match = path_match.replace("/etc/", "/private/etc/")
                 if EnvInfo.isDockerDesktop():
                     match = False
